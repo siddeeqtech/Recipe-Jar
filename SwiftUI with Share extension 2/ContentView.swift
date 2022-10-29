39f3d811b4618cceae87f62e0a6d68d0eff4b59b
othmansh0 <othmanshahrouri@gmail.com>
2022-10-29 19:01:46 +0200

    Initial Commit

---
+++ SwiftUI with Share extension 2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI with Share extension 2
+//
+//  Created by othman shahrouri on 10/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI with Share extension 2/SwiftUI_with_Share_extension_2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_with_Share_extension_2App.swift
+//  SwiftUI with Share extension 2
+//
+//  Created by othman shahrouri on 10/29/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_with_Share_extension_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
